e2e48f193a1195b4c111353ba69030c80a6a42f3
chenjunshen <022927@sichuanair.com>
2021-09-17 17:09:47 +0800

    增加双按钮弹窗实例

--- app/src/main/java/com/cjs/kotlinapp/MainActivity.java
+++ app/src/main/java/com/cjs/kotlinapp/MainActivity.java
@@ -4,21 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.cjs.widgets.dialog.MsgDialog;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
 
-        findViewById(R.id.btn_popWindow).setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new MsgDialog.Builder()
-                        .setMsg("这是一个测试的模态窗口")
+                        .setMsg("这是一个测试的单按钮模态窗口")
+                        .setCancelable(false)
+                        .setOutsideCancelable(false)
+                        .build().show(getSupportFragmentManager());
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MsgDialog.Builder()
+                        .setMsg("这是一个测试的双按钮模态窗口")
+                        .setDialogStyle(MsgDialog.Style.TWO_BUTTON)
                         .setCancelable(false)
                         .setOutsideCancelable(false)
                         .build().show(getSupportFragmentManager());
